9166a03588441ffef4c8e23309a8a4c00becae22
Namling <157800039+Namling9@users.noreply.github.com>
2024-03-25 07:45:26 +0000

    Day2

---
+++ day2/class2/members/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import path
+
+urlpatterns = [
+
+    path("", views.index, name = "index")
+]
--- day2/class2/members/views.py
+++ day2/class2/members/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    
+    context = {
+        "course_list" : ["Django", "python", "DSA", "Machine Learning", "Java"]
+
+        "student": [{"Name": "Namling Limbu", "Course":"Django"}, {"Name": "Ganga", "Course":"Machine Learning"} ]
+    }
+
+    return render(request, "index.html", context)
