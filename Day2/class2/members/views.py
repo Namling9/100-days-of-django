e4691317cfe10aa5660afe821d90b17d73dc7a93
Namling <157800039+Namling9@users.noreply.github.com>
2024-03-25 08:22:10 +0000

    Day2

--- Day2/class2/members/views.py
+++ Day2/class2/members/views.py
@@ -7,7 +7,9 @@ def index(request):
     context = {
         "course_list" : ["Django", "python", "DSA", "Machine Learning", "Java"],
 
-        "student": [{"Name": "Namling Limbu", "Course":"Django"}, {"Name": "Ganga", "Course":"Machine Learning"} ]
+        "student": [{"Name": "Namling Limbu", "Course":"Django"}, {"Name": "Ganga", "Course":"Machine Learning"} ],
+
+    
     }
 
     return render(request, "index.html", context)
